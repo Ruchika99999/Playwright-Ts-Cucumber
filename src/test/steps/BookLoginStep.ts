5c0f2896e891530da5f6fc8e00c02dfb1c891e94
Ruchika99999 <ruchikakumari1999@gmail.com>
2024-11-22 11:31:57 +0530

    Update BookLoginStep.ts

--- src/test/steps/BookLoginStep.ts
+++ src/test/steps/BookLoginStep.ts
@@ -11,13 +11,13 @@ setDefaultTimeout(60000);
   });
   
   Given('Enter the username as {string}',async function (username) {
-    await pageFixture.page.locator('input[placeholder="Username"]').type(username);
+    await pageFixture.page.getByRole('textbox', {name : 'Username'}).type(username);
       });
   
 
   Given('Enter the password as {string}',{ timeout: 60000}, async function (password) {
     console.log(" Third Assertion ");
-    await pageFixture.page.locator('input[placeholder="Password"]').type(password);
+    await pageFixture.page.getByRole('textbox', {name : 'Password'})..type(password);
   });
   When('Click on the login button', async function () {
     const value3=await pageFixture.page.locator('span.mdc-button__label:has-text("Login")').nth(1);
